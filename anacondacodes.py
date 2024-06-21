94e3bdcc5a87b466e4ba4c036262ed075a37e31f
ndala <ndalabuzangu@gmail.com>
2024-06-21 13:05:32 +0000

    Initial commit: Added initial project files and requirements.txt

---
+++ anacondacodes.py
@@ -0,0 +1,7 @@
+(base) C:\Users\user>conda activate ndalae
+
+(ndalae) C:\Users\user>pip freeze > requirements.txt
+
+(ndalae) C:\Users\user>notepad requirements.txt
+
+(ndalae) C:\Users\user>streamlit run "C:\Users\user\Documents\streamlitdeploy\overall performance player prediction web app.py"
